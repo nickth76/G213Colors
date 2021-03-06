1bdfbe4d1b41be95b9c00e8f3434cf00a9e6446f
NickTux <nicktux@ubuntu.desktop>
2021-03-06 23:01:18 +0100

    modified:   INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install libusb-1.0-0
-sudo apt-get install python3-gi python3-gi-cairo gir1.2-gtk-3.0
+sudo apt-get install python3-pip python3-gi python3-gi-cairo gir1.2-gtk-3.0
 sudo pip3 install pycairo PyGObject randomcolor pyusb
 sudo make install
 
